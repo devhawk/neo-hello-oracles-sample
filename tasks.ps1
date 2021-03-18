c230d39e3b96ba826b25417735616b33e598af69
Harry <harrypierson@outlook.com>
2021-03-17 17:26:59 -0700

    update for rc1

--- tasks.ps1
+++
@@ -1,8 +0,0 @@
-dotnet tool restore
-dotnet build ./contract/OracleSample.csproj
-
-dotnet nxp3 reset -f
-dotnet nxp3 oracle enable
-dotnet nxp3 contract deploy ./contract/bin/Debug/netstandard2.1/OracleSample.nef genesis
-dotnet nxp3 contract invoke ./make-oracle-call.neo-invoke.json genesis
-dotnet nxp3 oracle response fake:somepath.json ./response.json
